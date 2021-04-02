a1f82cc04b5bf003470ece6a539d56a1014556f0
khafni <hafnix@gmail.com>
2021-04-02 23:11:15 +0100

    ex05 complete

--- ex04/operations.py
+++ ex04/operations.py
@@ -1,9 +1,22 @@
 import sys
 
 def main():
-    if len(sys.argv) != 3:
-        print("ERROR")
-    
+	if len(sys.argv) != 3 or (not sys.argv[1].isdigit() or not sys.argv[2].isdigit()):
+		print("Usage: python operations.py <number1> <number2>")	
+		print("Example:")
+		print("   python operations.py 10 3")
+		return
+	n1 = int(sys.argv[1])
+	n2 = int(sys.argv[2])
+	print("Sum: {}".format(n1 + n2))
+	print("Difference: {}".format(n1 - n2))
+	print("Product: {}".format(n1 * n2))
+	if n2 == 0:
+		print("Quotient ERROR (div by zero)")
+		print("Remainder: ERROR(modulo by zero)")	
+		return
+	print("Quotient {}".format(float(n1) / float(n2)))
+	print("Remainder: {}".format(float(n1) % float(n2)))
 
 if __name__ == "__main__":
-    main()
+	main()
