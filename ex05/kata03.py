40c4e85e17f833c4a5b4b62da997628aa20b1a07
khafni <hafnix@gmail.com>
2021-04-03 10:40:23 +0100

    m01 ex05 complete

--- ex05/kata03.py
+++ ex05/kata03.py
@@ -1,5 +1,8 @@
 def main():
-    i = (0, 4, 132.42222, 10000, 12345.67)
-    print("day_{0:02}, ex_{1:02} : {2:.2f}, {3:.2e}, {4:.2e}".format(*i))
+    phrase = "The right format"
+    phrase = phrase.rjust(42, '-')
+    print(phrase, end='')
+
+
 if __name__ == "__main__":
     main()
