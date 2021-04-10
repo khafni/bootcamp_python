b732c9caa82306ab0a2c81f00fec34cf18e1d26e
pizzaboi <hafnix@gmail.com>
2021-04-10 01:06:42 +0100

    module01 ex00

--- module01/ex00/book.py
+++ module01/ex00/book.py
@@ -1,2 +1,18 @@
+from recipe import Recipe
+
 class Book:
-    def __init__()
+    def __init__(self, name, last_update, creation_date, recipe_list):
+        self.name = name 
+        self.last_update = last_update
+        self.creation_date = creation_date
+        self.recipe_list = recipe_list
+    def get_recipe_by_name(self, name):
+        for rec in self.recipe_list.values():
+            print(rec)
+
+r = Recipe("pizza", 1, 90, ["dough", "tomatoes sauce", "cheese"],"wow tasty", "italian food")
+b = Book(r.name, 1, 1, [].append(r))
+print(r)
+
+
+        
--- module01/ex00/recipe.py
+++ module01/ex00/recipe.py
@@ -1,7 +1,31 @@
 class Recipe:
-    def __init__(self, name = '', cooking_level = 1, cooking_time = 0, ingredients = [], decription = '', recipe_type = ''):
+    def __init__(self, name, cooking_level, cooking_time, ingredients, decription, recipe_type):
+        if not isinstance(name, str):
+            raise TypeError("name is not a string")
         self.name = name
+        if not isinstance(cooking_level, int):
+            raise TypeError("cooking_level is not an int")
+        self.cooking_level = cooking_level
+        if not isinstance(cooking_time, int):
+            raise TypeError("cooking_time is not an int")
+        self.cooking_time = cooking_time
+        if not isinstance(ingredients, list):
+            raise TypeError("ingredients is not a list")
+        self.ingredients = ingredients
+        if not isinstance(decription, str):
+            raise TypeError("description is not a string")
+        self.description = decription
+        if not isinstance(recipe_type, str):
+            raise TypeError("recipe_type is not a string")
+        self.recipe_type = recipe_type
 
+    def __str__(self):
+        str = ""
+        str += "Recipe name: {}\n".format(self.name)
+        str += "cooking level: {}\n".format(self.cooking_level)
+        str += "cooking time: {}\n".format(self.cooking_time)
+        str  += "ingredients: {}\n".format(" , ".join(self.ingredients))
+        str += "description: {}\n".format(self.description)
+        str += "recipe type: {}".format(self.recipe_type)
+        return str
 
-r = Recipe("lol")
-print(r.name)
