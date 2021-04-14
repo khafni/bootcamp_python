b31889d975697401c753291a6046071cc3e7fdd7
Khalil Hafni <khafni@e2r2p6.1337.ma>
2021-04-14 17:36:55 +0000

    ok

---
+++ module01/ex01/game.py
@@ -0,0 +1,9 @@
+class GotCharacter:
+    def __init__(self, first_name=None, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+class Stark(GotCharacter):
+    def __init__(self, first_name=first_name, )
+
+
